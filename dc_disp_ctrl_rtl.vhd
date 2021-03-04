8b4681966f4abbf7db9f219b464590a0459e40e3
vaad19uj <vaad19uj@student.ju.se>
2021-03-04 11:00:52 +0100

    Revert "Update dc_disp_ctrl_rtl.vhd"
    This reverts commit e5bc3c01f15e8b544adc0b7cbfdf9695ab9f408d.

--- dc_disp_ctrl_rtl.vhd
+++ dc_disp_ctrl_rtl.vhd
@@ -1,6 +1,5 @@
 library ieee;
 	use ieee.std_logic_1164.all;
-	use ieee.numeric_std.all;
 	
 
 entity dc_disp_ctrl is 
@@ -24,57 +23,20 @@ end entity dc_disp_ctrl;
 
 architecture rtl of dc_disp_ctrl is
 
-	-- types
+	-- type
 	type natural_array is array (2 downto 0) of natural;
 	
 	-- signals
-	signal current_dc 				: natural_array;
-	signal ASCII_dc_0 				: std_logic_vector(7 downto 0);
-	signal ASCII_dc_1 				: std_logic_vector(7 downto 0);
-	signal ASCII_dc_2 				: std_logic_vector(7 downto 0);
-	
-	-- constants
-	constant percent		 			: std_logic_vector(7 downto 0) := "00100101";	--37 decimal
-	constant space 		 			: std_logic_vector(7 downto 0) := "00100000";	-- 32 decimal
-	constant carriage_return 		: std_logic_vector(7 downto 0) := "00001101";	-- 13 decimal
+	signal current_dc 		: natural_array;
+	signal ASCII_dc_0 		: std_logic_vector(7 downto 0);
+	signal ASCII_dc_1 		: std_logic_vector(7 downto 9);
+	signal ASCII_dc_2 		: 
 
 	begin
-	
-	-- hex2 - 0 eller 1
-	-- hex1 - 0-9
-	-- hex0 - 0-9
 	
 		current_dc(0) 			<= current_dc0;
 		current_dc(1) 			<= current_dc1;
 		current_dc(2) 			<= current_dc2;
-		
-		p_dc : process(reset)
-		begin
-		
---			The duty cycle shall also be trasmitted on the serial interface whenever the duty cycle is updated. 
---			The transmitted data shall be five bytes of data. Three ASCII characters representing the duty cycle between 0 and 100 
---			followed by a ‘%’ character, followed by a carrage return.  
---			In the case of a duty cycle between 10 and 99 the first character shall be replaced with a space. 
---			And in the case when the duty cycle is between 0 and 9 the first two characters shall be space. 
---			In the case of a new duty cycle update have been reported before the current duty cycle information 
---			have been fully transmitted on the serial interface the serial send shall be directly started 
---			again when finished in order to update the serial interface with the latest information. 
-
-
-			if transmit_ready = '1' then 
-			
-			-- 0-9
-				transmit_data <= space & space & ASCII_dc_0 & percent & carriage_return;
-			
-			-- 10-99
-				transmit_data <= space & ASCII_dc_1 & ASCII_dc_0 & percent & carriage_return;
-			
-			-- 100
-				transmit_data <= ASCII_dc_2 & ASCII_dc_1 & ASCII_dc_0 & percent & carriage_return;
-			
-			end if;
-			
-		end process p_dc;
 		
 		p_current_dc0 : process(clk, reset)
 		begin 
@@ -83,49 +45,38 @@ architecture rtl of dc_disp_ctrl is
 				if reset = '1' then
 					-- zero
 					hex0 <= (others => '1');
-					ASCII_dc_0 <= "00110000";
 					
 				else
 					case current_dc(0) is
 						when 0 =>
 						-- zero
-							ASCII_dc_0 <= "00110000";
 							hex0 <= (6 => '1', others => '0');
 						when 1 =>
 						-- one
-							ASCII_dc_0 <= "00110001";
 							hex0 <= (1 => '0', 2 => '0', others => '1');
 						when 2 =>
-						-- two 
-							ASCII_dc_0 <= "00110010";
+						-- two
 							hex0 <= (0 => '0', 1 => '0', 6 => '0', 4 => '0', 3 => '0', others => '1');
 						when 3 =>
 						-- three
-							ASCII_dc_0 <= "00110011";
 							hex0 <= (0 => '0', 1 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
 						when 4 =>
 						-- four
-							ASCII_dc_0 <= "00110100";
 							hex0 <= (5 => '0', 6 => '0', 1 => '0', 2 => '0', others => '1');
 						when 5 =>
 						-- five
-							ASCII_dc_0 <= "00110101";
 							hex0 <= (0 => '0', 5 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
 						when 6 =>
 						-- six
-							ASCII_dc_0 <= "00110110";
 							hex0 <= (0 => '0', 5 => '0', 6 => '0', 4 => '0', 3 => '0', 2 => '0', others => '1');
 						when 7 =>
 						-- seven
-							ASCII_dc_0 <= "00110111";
 							hex0 <= (0 => '0', 1 => '0', 2 => '0', others => '1');
 						when 8 =>
 						-- eight
-							ASCII_dc_0 <= "00111000";
 							hex0 <= (others => '0');
 						when 9 =>
 						-- nine
-							ASCII_dc_0 <= "00111001";
 							hex0 <= (4 => '1', others => '0');
 						when others =>
 							-- turned off
@@ -143,49 +94,38 @@ architecture rtl of dc_disp_ctrl is
 				if reset = '1' then
 					-- zero
 					hex1 <= (others => '1');
-					ASCII_dc_1 <= "00110000";
 					
 				else
-					case current_dc(2) is
+					case current_dc(1) is
 						when 0 =>
 						-- zero
-							ASCII_dc_1 <= "00110000";
 							hex1 <= (6 => '1', others => '0');
 						when 1 =>
 						-- one
-							ASCII_dc_1 <= "00110001";
 							hex1 <= (1 => '0', 2 => '0', others => '1');
 						when 2 =>
-						-- two 
-							ASCII_dc_1 <= "00110010";
+						-- two
 							hex1 <= (0 => '0', 1 => '0', 6 => '0', 4 => '0', 3 => '0', others => '1');
 						when 3 =>
 						-- three
-							ASCII_dc_1 <= "00110011";
 							hex1 <= (0 => '0', 1 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
 						when 4 =>
 						-- four
-							ASCII_dc_1 <= "00110100";
 							hex1 <= (5 => '0', 6 => '0', 1 => '0', 2 => '0', others => '1');
 						when 5 =>
 						-- five
-							ASCII_dc_1 <= "00110101";
 							hex1 <= (0 => '0', 5 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
 						when 6 =>
 						-- six
-							ASCII_dc_1 <= "00110110";
 							hex1 <= (0 => '0', 5 => '0', 6 => '0', 4 => '0', 3 => '0', 2 => '0', others => '1');
 						when 7 =>
 						-- seven
-							ASCII_dc_1 <= "00110111";
 							hex1 <= (0 => '0', 1 => '0', 2 => '0', others => '1');
 						when 8 =>
 						-- eight
-							ASCII_dc_1 <= "00111000";
 							hex1 <= (others => '0');
 						when 9 =>
 						-- nine
-							ASCII_dc_1 <= "00111001";
 							hex1 <= (4 => '1', others => '0');
 						when others =>
 							-- turned off
@@ -202,19 +142,44 @@ architecture rtl of dc_disp_ctrl is
 
 				if reset = '1' then
 					-- zero
-					ASCII_dc_2 <= "00110000";
 					hex2 <= (others => '1');
 					
 				else
-					if current_dc(2) = 1 then 
-						-- one
-						ASCII_dc_2 <= "00110001";
-						hex2 <= (1 => '0', 2 => '0', others => '1');
-					else
+					case current_dc(2) is
+						when 0 =>
 						-- zero
-						ASCII_dc_2 <= "00110000";
-						hex2 <= (6 => '1', others => '0');
-					end if;
+							hex2 <= (6 => '1', others => '0');
+						when 1 =>
+						-- one
+							hex2 <= (1 => '0', 2 => '0', others => '1');
+						when 2 =>
+						-- two
+							hex2 <= (0 => '0', 1 => '0', 6 => '0', 4 => '0', 3 => '0', others => '1');
+						when 3 =>
+						-- three
+							hex2 <= (0 => '0', 1 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
+						when 4 =>
+						-- four
+							hex2 <= (5 => '0', 6 => '0', 1 => '0', 2 => '0', others => '1');
+						when 5 =>
+						-- five
+							hex2 <= (0 => '0', 5 => '0', 6 => '0', 2 => '0', 3 => '0', others => '1');
+						when 6 =>
+						-- six
+							hex2 <= (0 => '0', 5 => '0', 6 => '0', 4 => '0', 3 => '0', 2 => '0', others => '1');
+						when 7 =>
+						-- seven
+							hex2 <= (0 => '0', 1 => '0', 2 => '0', others => '1');
+						when 8 =>
+						-- eight
+							hex2 <= (others => '0');
+						when 9 =>
+						-- nine
+							hex2 <= (4 => '1', others => '0');
+						when others =>
+							-- turned off
+							hex2 <= (others => '1');
+					end case;
 				end if;
 			end if;
 		end process p_current_dc2;
