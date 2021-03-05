c1bcab5aa998c9dfd51ad1e2341829af92b7d82a
vaad19uj <vaad19uj@student.ju.se>
2021-03-05 15:21:45 +0100

    Update key_ctrl_rtl.vhd

--- key_ctrl_rtl.vhd
+++ key_ctrl_rtl.vhd
@@ -5,7 +5,7 @@ library ieee;
 entity key_ctrl is 
 	port(
 		key_n 			: in std_logic_vector(3 downto 0);
-		clock 			: in std_logic;
+		clock 			: in std_logic; -- 50MHz
 		key_on 			: out std_logic;
 		key_off 			: out std_logic;
 		key_down 		: out std_logic;
@@ -16,47 +16,83 @@ end entity key_ctrl;
 architecture rtl of key_ctrl is
 
 	-- signals
-	signal key_in_r : std_logic_vector(3 downto 0);
-	signal key_in_2r : std_logic_vector(3 downto 0);
+	signal key_in_r 		: std_logic_vector(3 downto 0);
+	signal key_in_2r 		: std_logic_vector(3 downto 0);
+	signal ticks			: integer range 0 to 500000;
 
 begin
 	
-	p_double_sync : process(key_n)
+	p_double_sync : process(clock)
 	begin
 		key_in_r <= key_n;
 		key_in_2r <= key_in_r;
 	end process p_double_sync;
 	
 	p_key_ctrl : process(clock)
+
 	begin
 		if rising_edge(clock) then
+		ticks <= 0;
 		
-			--	Key_n input bits 3, 2 and 1 shall be ignored if key_n(0) is pushed down.
 			if key_in_2r(0) = '0' then
-			
-			--	No pulses on key_up or key_down shall be generated if both key_n(2) and key_n(3) is pushed down simultaneously. 
-			elsif key_in_2r(2) = '0' and key_in_2r(3) = '0' then
+				key_off <= '1';
+				key_down <= 'X';
+				key_up <= 'X';
+				key_on <= 'X';
 				
-			else
+				if(ticks = 500000) then
+					key_off <= '1';
+					ticks <= 0;
+				else 
+					ticks <= ticks +1;
+					key_off <= '0';
+				end if;
 			
+			elsif key_in_2r(2) = '0' and key_in_2r(3) = '0' then
+				key_down <= 'X';
+				key_up <= 'X';
+				
+				
+			elsif key_in_2r(1) = '0' then
+				key_on <= '1';
+					if(ticks = 500000) then
+						key_on <= '1';
+						ticks <= 0;
+					else 
+						ticks <= ticks +1;
+						key_on <= '0';
+					end if;
+					
+			elsif key_in_2r(2) = '0' then
+				key_down <= '1';
+				
+				if(ticks = 500000) then
+					key_down <= '1';
+					ticks <= 0;
+				else 
+					ticks <= ticks +1;
+					key_down <= '0';
+				end if;
+					
+					
+			elsif key_in_2r(3) = '0' then
+				key_up <= '1';
+				
+				if(ticks = 500000) then
+					key_up <= '1';
+					ticks <= 0;
+				else 
+					ticks <= ticks +1;
+					key_up <= '0';
+				end if;
+				
+			else 
 				key_off <= not key_in_2r(0);
-				key_off <= not key_in_2r(1);
+				key_on <= not key_in_2r(1);
 				key_down <= not key_in_2r(2);
 				key_up <= not key_in_2r(3);
 			
 			end if;
-			
-		
---			key_n(0) shall control key_off output 
---			key_n(1) shall control key_on output 
---			key_n(2) shall control key_down output 
---			key_n(3) shall control key_up output 
---			Key_n input bits 3, 2 and 1 shall be ignored if key_n(0) is pushed down. 
---			No pulses on key_up or key_down shall be generated if both key_n(2) and key_n(3) is pushed down simultaneously. 
---			The outputs from the compoent shall be set high one clock cycle if the inputs 
---			are detected to be low. If the inputs are held low the outputs shall be 
---			pulsed high one clock cycle every 10th millisecond. 
-
 		end if;
 	end process p_key_ctrl;
 
